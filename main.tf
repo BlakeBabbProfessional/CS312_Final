35c55aad5c8cc35da30bab46d7e6f6b38170e21f
blakebabb <leopard50x1@gmail.com>
2023-06-11 16:26:13 -0700

    Added security group, init_script

--- main.tf
+++ main.tf
@@ -13,12 +13,34 @@ provider "aws" {
   region  = "us-west-2"
 }
 
+provider "random" {}
+
+resource "random_pet" "name" {}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.small"
+  user_data		= file("init_script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = random_pet.name.id
   }
 }
 
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.name.id}-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
