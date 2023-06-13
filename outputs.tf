3fd9fec6f1174e378e92dffe4d73a99a5f92ed19
blakebabb <leopard50x1@gmail.com>
2023-06-13 13:48:23 -0700

    outputs + key files (I know it's bad to commit these, but ehhhhhhh)

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,7 @@ output "instance_public_ip" {
   value       = aws_instance.app_server.public_ip
 }
 
+output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value		  = aws_instance.app_server.public_dns
+}
